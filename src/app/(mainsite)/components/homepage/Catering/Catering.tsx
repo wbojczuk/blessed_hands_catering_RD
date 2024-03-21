d85aa82fde7f78d81487bbfd902f6ebcedd9b3b0
William Bojczuk <williambojczuk@gmail.com>
2024-03-21 11:15:39 +0200

    responsive homepage done

--- src/app/(mainsite)/components/homepage/Catering/Catering.tsx
+++ src/app/(mainsite)/components/homepage/Catering/Catering.tsx
@@ -6,7 +6,7 @@ export default function Catering() {
   return (
     <section className={styles.section}>
         <div className={styles.imgWrapper}>
-            <img className={styles.img} src="/img/catering-bg.png" width={360} height={600} alt="Featured Service Image" />
+            <img className={styles.img} src="/img/catering-bg.png" width={360} height={600} alt="Image of catering food" />
         </div>
         
 
@@ -14,9 +14,9 @@ export default function Catering() {
             <h4>Blessed Hands</h4>
             <h2>Catering And Events</h2>
 
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem suscipit, nam aliquid vero tenetur doloribus quas distinctio maxime doloremque et. Consequatur, odio velit ipsam quod placeat accusamus mollitia optio laudantium?
-            <br /><br />
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, nihil. Sit, animi similique voluptatibus corporis saepe aliquam accusantium autem sunt quasi ut pariatur, eum dolorum soluta ad obcaecati, asperiores et.
+            <p>Whether you're planning a corporate event, a wedding celebration, or an intimate gathering with friends, Blessed Hands Catering is here to cater to your every need.
+              <br /><br />
+              From carefully curated menus to the flawless delivery, we ensure that every detail is tailored to perfection, leaving you free to breathe in the moment.
             </p>
             <Link className='main-link' href='/contact'><span>Inquire</span><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="m14.707 5.636l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-1.414-1.414l3.95-3.95H4a1 1 0 1 1 0-2h13.243l-3.95-3.95a1 1 0 1 1 1.414-1.414"/></g></svg></Link>
         
--- src/app/(mainsite)/components/homepage/Unique/Unique.tsx
+++ src/app/(mainsite)/components/homepage/Unique/Unique.tsx
@@ -6,7 +6,7 @@ export default function Unique() {
   return (
     <section className={styles.section}>
         <div className={styles.imgWrapper}>
-            <img className={styles.img} src="/img/catering-bg.png" width={360} height={600} alt="Featured Service Image" />
+            <img className={styles.img} src="/img/food-on-shelf.png" width={360} height={600} alt="image of food on shelf" />
         </div>
         
 
@@ -14,9 +14,9 @@ export default function Unique() {
             <h4>Keep It Unique</h4>
             <h2>Make It Yours!</h2>
 
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem suscipit, nam aliquid vero tenetur doloribus quas distinctio maxime doloremque et. Consequatur, odio velit ipsam quod placeat accusamus mollitia optio laudantium?
+            <p>We understand that dietary preferences and restrictions vary among our clients. That's why we proudly offer custom menus for each event, completely free of charge. We're dedicated to accommodating your needs without any additional fees.
             <br /><br />
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, nihil. Sit, animi similique voluptatibus corporis saepe aliquam accusantium autem sunt quasi ut pariatur, eum dolorum soluta ad obcaecati, asperiores et.
+            When you choose us for your event, you can be sure that every detail will be handled with care and attention, leaving you free to be in the moment and enjoy the experience.
             </p>
             <Link className='main-link' href='/contact'><span>Inquire</span><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="m14.707 5.636l5.657 5.657a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 0 1-1.414-1.414l3.95-3.95H4a1 1 0 1 1 0-2h13.243l-3.95-3.95a1 1 0 1 1 1.414-1.414"/></g></svg></Link>
             
